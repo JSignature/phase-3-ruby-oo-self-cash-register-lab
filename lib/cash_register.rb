98f0480158832c5a0c70abd9c0d84b095f6741c4
Jimmy Andreas <jimmysignature@gmail.com>
2022-07-20 21:28:00 -0400

    Need last 4 tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require "pry"
 
+
+class CashRegister 
+
+attr_reader 
+attr_writer
+attr_accessor :discount, :total 
+
+
+def initialize (discount=0)
+@total = 0
+@discount = discount 
+
+end
+
+def add_item (title, price, quantity=1)
+    self.items(title)
+    sum = price * quantity
+self.total = @total += sum
+end
+
+def apply_discount
+    if @discount > 0 
+disc = @discount.to_f / 100
+sum = @total * disc
+@total = self.total - sum
+"After the discount, the total comes to $#{@total.to_i}."
+    else
+        "There is no discount to apply."
+    end
+end
+
+def items (item)
+    @items_arr = []
+    @items_arr.push(item)
+    @items_arr
+    binding.pry
+   
+    
+end
+
+    
+
+end
